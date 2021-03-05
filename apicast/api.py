eb76314f1d8255117552772db43fd27633f0717f
Andreas Motl <andreas@hiveeyes.org>
2021-03-05 03:11:13 +0100

    Adjust HTTP API

--- apicast/api.py
+++ apicast/api.py
@@ -1,26 +1,52 @@
 # -*- coding: utf-8 -*-
-# (c) 2020 Andreas Motl <andreas@terkin.org>
+# (c) 2020-2021 Andreas Motl <andreas@hiveeyes.org>
 # License: GNU Affero General Public License, Version 3
 import logging
-from copy import deepcopy
 
 from fastapi import FastAPI, Query
 from fastapi.responses import HTMLResponse, PlainTextResponse
 
 from apicast import __appname__, __version__
-from apicast.core import dwd_beeflight_forecast_stations, dwd_beeflight_forecast_stations_site_slugs, \
-    dwd_beeflight_site_url_by_slug, dwd_beeflight_forecast_data, dwd_source, dwd_copyright, producer_name, producer_link
+from apicast.core import dwd_source, dwd_copyright, producer_name, producer_link, DwdBeeflightForecast
 from apicast.format import Formatter
 
 app = FastAPI()
 
 log = logging.getLogger(__name__)
 
+dbf = DwdBeeflightForecast()
+
 
 @app.get("/", response_class=HTMLResponse)
 def index():
     appname = f'{__appname__} {__version__}'
     about = 'Apicast acquires bee flight forecast information published by Deutscher Wetterdienst (DWD).'
+
+    data_index_items = []
+    for location in dbf.get_station_slugs():
+        item = f'''
+            <li>
+                <div style="float: left">
+                Forecast for {location}
+                </div>
+                <div style="float: right">
+                German:
+                    <a href="beeflight/forecast/germany/{location}?translate=false&format=json">[JSON]</a>
+                    <a href="beeflight/forecast/germany/{location}?translate=false&format=table-markdown">[Markdown]</a>
+                English:
+                    <a href="beeflight/forecast/germany/{location}?translate=true&format=json">[JSON]</a>
+                    <a href="beeflight/forecast/germany/{location}?translate=true&format=table-markdown">[Markdown]</a>
+
+                Machine:
+                    <a href="beeflight/forecast/germany/{location}?format=json-machine">[JSON]</a>
+                </div>
+                <div style="clear: both"/>
+            </li>
+        '''
+        data_index_items.append(item)
+
+    data_index_items_html = "\n".join(data_index_items)
+
     return f"""
     <html>
         <head>
@@ -30,19 +56,18 @@ def index():
             <h3>About</h3>
             {about}
             <ul>
-            <li>Source: DWD » Freizeitgärtner » Gartenwetter - <a href="{dwd_source}">{dwd_source}</a></li>
-            <li>Producer: {producer_name} - <a href="{producer_link}">{producer_link}</a></li>
+            <li>Source: <a href="{dwd_source}">DWD » Freizeitgärtner » Gartenwetter » Prognose des Bienenfluges</a></li>
+            <li>Producer: <a href="{producer_link}">{producer_name}</a></li>
             <li>Data copyright: {dwd_copyright}</li>
             </ul>
-            <h3>Index</h3>
+            <h3>Location index</h3>
             <ul>
-            <li><a href="beeflight/stations/germany">List of federal states / sites</a></li>
-            <li><a href="beeflight/stations/germany/site-slugs">List of site slugs</a></li>
+            <li><a href="beeflight/stations/germany">List of federal states / stations</a></li>
+            <li><a href="beeflight/stations/germany/locations">List of location slugs</a></li>
             </ul>
-            <h3>Examples</h3>
-            <ul>
-            <li><a href="beeflight/forecast/germany/berlin_brandenburg/potsdam">Bee flight forecast for "berlin_brandenburg/potsdam"</a></li>
-            <li><a href="beeflight/forecast/germany/bayern/regensburg">Bee flight forecast for "bayern/regensburg"</a></li>
+            <h3>Data index</h3>
+            <ul style="width: 65%">
+            {data_index_items_html}
             </ul>
         </body>
     </html>
@@ -59,25 +84,25 @@ Disallow: /beeflight/
 
 @app.get("/beeflight/stations/germany")
 def beeflight_stations():
-    stations = dwd_beeflight_forecast_stations()
+    stations = dbf.get_stations()
     return make_json_response(stations)
 
 
-@app.get("/beeflight/stations/germany/site-slugs")
+@app.get("/beeflight/stations/germany/locations")
 def beeflight_stations_site_slugs():
-    slugs = dwd_beeflight_forecast_stations_site_slugs()
+    slugs = dbf.get_station_slugs()
     return make_json_response(slugs)
 
 
-@app.get("/beeflight/forecast/germany/{state}/{site}")
-def beeflight_forecast_by_slug(state: str, site: str, format: str = Query(default='json'), translate: bool = Query(default=False)):
-    station_slug = f"{state}/{site}"
+@app.get("/beeflight/forecast/germany/{state}/{station}")
+def beeflight_forecast_by_slug(state: str, station: str, format: str = Query(default='json'), translate: bool = Query(default=False)):
+
+    station_slug = f"{state}/{station}"
 
     try:
-        url = dwd_beeflight_site_url_by_slug(station_slug)
-        result = deepcopy(dwd_beeflight_forecast_data(url))
-        data = result['data']
-        if not data:
+        station = dbf.get_station_by_slug(station_slug)
+        result = dbf.get_data(station=station).copy()
+        if not result.data:
             raise ValueError('No data found or unable to parse')
     except Exception as ex:
         return {'error': str(ex)}
@@ -96,15 +121,18 @@ def beeflight_forecast_by_slug(state: str, site: str, format: str = Query(defaul
     else:
         response = formatter.normalize()
 
-    return make_json_response(response)
+    return make_json_response(location=station_slug, data=response)
 
 
-def make_json_response(data):
+def make_json_response(location: str, data: list[dict]):
     response = {
         'meta': {
             'source': dwd_source,
-            'copyright': dwd_copyright,
             'producer': f'{producer_name} - {producer_link}',
+            'copyright': dwd_copyright,
+        },
+        'location': {
+            'slug': location,
         },
         'data': data,
     }
