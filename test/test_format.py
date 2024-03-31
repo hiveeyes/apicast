65eea66bd228c729c4343a4f501a5ab4836e1272
Andreas Motl <andreas@hiveeyes.org>
2024-03-31 06:18:11 +0200

    Fix translation "sehr hoch" => "intensive"

---
+++ test/test_format.py
@@ -0,0 +1,51 @@
+import pytest
+
+from apicast.core import Result, Station, State
+from apicast.format import Formatter
+
+
+sample_result = Result(
+    station=Station(
+        state=State(label='Brandenburg', identifier='bifl_bl04'), label='Potsdam', identifier='bifl_0021',
+        slug='brandenburg/potsdam'), station_name='Potsdam',
+    data=[
+        ['Datum', 'morgens', 'mittags', 'abends'],
+        ['Sa 30.03.', 'mittel', 'sehr hoch', 'hoch'],
+        ['So 31.03.', 'mittel', 'hoch', 'hoch'],
+        ['Mo 01.04.', 'mittel', 'hoch', 'mittel'],
+    ],
+    footnote='© Deutscher Wetterdienst, erstellt 30.03.2024 05:00 UTC. Alle Angaben ohne Gewähr!',
+)
+
+
+@pytest.fixture
+def formatter() -> Formatter:
+    return Formatter(sample_result)
+
+
+def test_format_basic(formatter: Formatter):
+    assert formatter.title == "### Prognose des Bienenfluges in Potsdam"
+    assert len(formatter.data) == 4
+
+
+def test_format_normalize(formatter: Formatter):
+    assert formatter.normalize() == [
+        {'Datum': 'Sa 30.03.', 'morgens': 'mittel', 'mittags': 'sehr hoch', 'abends': 'hoch'},
+        {'Datum': 'So 31.03.', 'morgens': 'mittel', 'mittags': 'hoch', 'abends': 'hoch'},
+        {'Datum': 'Mo 01.04.', 'morgens': 'mittel', 'mittags': 'hoch', 'abends': 'mittel'}]
+
+
+def test_format_machinify(formatter: Formatter):
+    assert formatter.machinify() == [{'date': '2024-03-30', 'evening': 3, 'morning': 2, 'noon': 4},
+                                     {'date': '2024-03-31', 'evening': 3, 'morning': 2, 'noon': 3},
+                                     {'date': '2024-04-01', 'evening': 2, 'morning': 2, 'noon': 3}]
+
+
+def test_format_translate(formatter: Formatter):
+    formatter.translate()
+    assert formatter.data == [['date', 'morning', 'noon', 'evening'], ['Sa 30.03.', 'medium', 'intensive', 'strong'],
+                              ['So 31.03.', 'medium', 'strong', 'strong'], ['Mo 01.04.', 'medium', 'strong', 'medium']]
+
+
+def test_format_markdown(formatter: Formatter):
+    assert "sehr strong" not in formatter.table_markdown()
